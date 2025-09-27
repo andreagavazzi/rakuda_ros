0e9af1dbbbd78872372e2524e6b4233bd37b7573
Andrea Gavazzi <andrea.gavazzi@me.com>
2025-09-28 01:47:11 +0200

    Update head_camera.launch.py

--- rakuda_vision/launch/head_camera.launch.py
+++ rakuda_vision/launch/head_camera.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Andrea Gavazzi
+# Copyright 2025 Andrea Gavazzi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,32 +14,48 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+    declare_publish_tf = DeclareLaunchArgument(
+        'publish_tf', default_value='false', description='Whether to publish TFs from camera'
+    )
+
     head_camera_node = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                get_package_share_directory('realsense2_camera'),
-                '/launch/rs_launch.py']),
-            launch_arguments={
-                'camera_namespace': '',
-                'camera_name': 'head_camera',
-                'device_type': 'd415',
-                'rgb_camera.color_profile': '640x360x30',
-                'depth_module.depth_profile': '640x360x30',
-                'pointcloud.enable': 'true',
-                'align_depth.enable': 'true',
-                
-                'base_frame_id': 'head_camera_link',
-                'color_frame_id': 'head_camera_color_frame',
-                'color_optical_frame_id': 'head_camera_color_optical_frame',
-                'publish_tf': 'false'   # tf are published by robot_state_publisher
-                
-            }.items()
-        )
+        PythonLaunchDescriptionSource([
+            get_package_share_directory('realsense2_camera'),
+            '/launch/rs_launch.py'
+        ]),
+        launch_arguments={
+            'camera_namespace': '',
+            'camera_name': 'head_camera',
+            'device_type': 'd415',
+
+            # Profili corretti all’avvio
+            'rgb_camera.color_profile': '640x360x30',
+            'depth_module.depth_profile': '640x360x30',
+            'infra_module.infra_profile': '640x360x30',
+            'depth_module.infra_profile': '640x360x30',
+
+            # Abilitazioni
+            'infra1.enable': 'false',
+            'infra2.enable': 'false',
+            'depth_module.visual_preset': '0',  # default preset, evita modifiche automatiche
+            
+
+            # Parametri opzionali
+            'pointcloud.enable': 'true',
+            'align_depth.enable': 'true',
+            'publish_tf': LaunchConfiguration('publish_tf'),
+
+            'color_frame_id': 'head_camera_color_frame',
+            'color_optical_frame_id': 'head_camera_color_optical_frame'
+        }.items()
+    )
 
     return LaunchDescription([
+        declare_publish_tf,
         head_camera_node
     ])
