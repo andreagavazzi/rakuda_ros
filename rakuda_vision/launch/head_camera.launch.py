312637e4070d0a5c1a2c01bc7921a607450a5beb
Andrea Gavazzi <andrea.gavazzi@me.com>
2025-09-28 23:25:06 +0200

    Update head_camera.launch.py

--- rakuda_vision/launch/head_camera.launch.py
+++ rakuda_vision/launch/head_camera.launch.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
