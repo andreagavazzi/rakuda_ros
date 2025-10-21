09fee6d5bb8c9e692e4a67a48bae8c588df8a3f0
Andrea Gavazzi <andrea.gavazzi@me.com>
2025-10-21 22:31:23 +0200

    Remove unnecessary comments and docstring

--- face_detection/face_detection/__init__.py
+++ face_detection/face_detection/__init__.py
@@ -1,9 +1,1 @@
-"""
-face_detection_pkg
 
-Pacchetto ROS 2 per face detection con OpenCV DNN (ottimizzato per Jetson Orin Nano)
-"""
-
-# Questo file rende la directory 'face_detection_pkg' un pacchetto Python.
-# Non è necessario aggiungere altro qui, a meno che tu non voglia
-# esportare funzioni o classi specifiche.
