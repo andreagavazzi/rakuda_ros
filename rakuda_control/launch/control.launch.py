b2719542cdcc1e6e906720caaf1768b0a92ad3a9
Andrea Gavazzi <andrea.gavazzi@me.com>
2026-02-09 21:46:00 +0100

    Add xacro processing for robot description in launch file

--- rakuda_control/launch/control.launch.py
+++ rakuda_control/launch/control.launch.py
@@ -1,20 +1,33 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import xacro
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
+    # 1. Recupera i path
     control_share = get_package_share_directory('rakuda_control')
+    description_share = get_package_share_directory('rakuda_description') # Necessario per lo xacro
+    
+    # 2. Processa lo Xacro (uguale a description.launch.py)
+    xacro_file = os.path.join(description_share, 'urdf', 'rakuda.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
     controllers_file = os.path.join(control_share, 'config', 'rakuda_controllers.yaml')
     
+    # 3. Passa robot_description come PARAMETRO, non solo remapping
     ros2_control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        parameters=[controllers_file],
-        remappings=[('~/robot_description', '/robot_description')],
+        parameters=[
+            robot_description,    # <--- FONDAMENTALE
+            controllers_file
+        ],
         output='log',
     )
+    
 
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
