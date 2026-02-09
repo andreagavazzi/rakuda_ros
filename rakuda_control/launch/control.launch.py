de26c07e7ba4efa9dbf247394cf93e9036c9086f
Andrea Gavazzi <andrea.gavazzi@me.com>
2026-02-09 22:17:51 +0100

    refactored

--- rakuda_control/launch/control.launch.py
+++ rakuda_control/launch/control.launch.py
@@ -1,71 +1,86 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import xacro
-import os
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
+import os
+import xacro
 
 def generate_launch_description():
 
-    # 1. Recupera i path
+    # Recupera path e xacro
     control_share = get_package_share_directory('rakuda_control')
-    description_share = get_package_share_directory('rakuda_description') # Necessario per lo xacro
-    
-    # 2. Processa lo Xacro (uguale a description.launch.py)
+    description_share = get_package_share_directory('rakuda_description')
     xacro_file = os.path.join(description_share, 'urdf', 'rakuda.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_description = {'robot_description': robot_description_config.toxml()}
-
+    doc = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': doc.toxml()}
     controllers_file = os.path.join(control_share, 'config', 'rakuda_controllers.yaml')
-    
-    # 3. Passa robot_description come PARAMETRO, non solo remapping
+
+    # 1. Controller Manager 
+    # Il nodo principale, deve partire prima di tutto
     ros2_control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        parameters=[
-            robot_description,    # <--- FONDAMENTALE
-            controllers_file
-        ],
+        parameters=[robot_description, controllers_file],
         output='log',
     )
-    
 
+    # 2. Joint State Broadcaster
+    # Parte subito insieme al manager dato che ha un timeout interno per gestisce l'attesa
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        name='spawner_joint_state_broadcaster',
-        arguments=['joint_state_broadcaster', '-c', '/controller_manager', '--controller-manager-timeout', '60'],
+        arguments=['joint_state_broadcaster'],
         output='log'
     )
 
+    # 3. Spawner Torso
     torso_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        name='spawner_torso_controller',
-        arguments=['torso_controller', '-c', '/controller_manager', '--controller-manager-timeout', '60'],
+        arguments=['torso_controller'],
         output='log'
     )
 
+    # 4. Spawner Head
     head_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        name='spawner_head_controller',
-        arguments=['head_controller', '-c', '/controller_manager', '--controller-manager-timeout', '60'],
+        arguments=['head_controller'],
         output='log'
     )
 
-    # Disabilita torque dai Dynamixel delle braccia
-    torque_except_node = Node(
-        package="rakuda_tools",
-        executable="torque_except",
-        name="torque_except",
-        output="log",
+    #left_arm_controller_spawner = Node(
+    #    package='controller_manager',
+    #    executable='spawner',
+    #    arguments=['left_arm_controller'],
+    #    output='log'
+    #)
+    
+    #right_arm_controller_spawner = Node(
+    #    package='controller_manager',
+    #    executable='spawner',
+    #    arguments=['right_arm_controller'],
+    #    output='log'
+    #)
+
+    # 5. Catena eventi
+    # Quando joint_state_broadcaster finisce (esce), allora avvia i controller
+    delay_controllers_after_joint_state = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[
+                torso_controller_spawner,
+                head_controller_spawner,
+                #left_arm_controller_spawner,
+                #right_arm_controller_spawner,
+            ]
+        )
     )
 
+    # 6. Return
     return LaunchDescription([
-        ros2_control_node,
-        joint_state_broadcaster_spawner,
-        torso_controller_spawner,
-        head_controller_spawner,
-        torque_except_node,
+        ros2_control_node, # Parte subito
+        joint_state_broadcaster_spawner, # Parte subito (ha già un timeout interno che aspetta il manager)
+        delay_controllers_after_joint_state, # Contiene gli spawner dei controller che partono dopo il joint_state_broadcaster
     ])
-
